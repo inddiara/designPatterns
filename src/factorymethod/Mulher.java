bfbe746616207637f6bc506634451720eb46f2cc
Indiara Duarte <indiara96d@gmail.com>
2018-10-25 17:54:49 -0300

    Factory method example

---
+++ src/factorymethod/Mulher.java
@@ -0,0 +1,8 @@
+package factorymethod;
+
+public class Mulher extends Pessoa {
+	public Mulher(String nome) {
+        this.nome = nome;
+        System.out.println("Olá Senhora" + this.nome);
+    }
+}
---
+++ src/factorymethod/Pessoa.java
@@ -0,0 +1,7 @@
+package factorymethod;
+
+public abstract class Pessoa {
+	public String nome;
+    public String sexo;
+    
+}
