d48cd7b88957e6d27567eb105fc27c6124f6d62f
Indiara Duarte <indiara96d@gmail.com>
2018-10-25 17:58:33 -0300

    Adapter example

---
+++ src/adapter/App.java
@@ -0,0 +1,10 @@
+package adapter;
+
+public class App {
+	public static void main(String args[]) {
+        TomadaDeTresPinos t3 = new TomadaDeTresPinos();
+         
+        AdapterTomada a = new AdapterTomada(t3);
+        a.ligarNaTomadaDeDoisPinos();
+    }
+}
